dfcf2d65d9b33c14d61bfd680e629184879ff808
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-01-19 21:19:20 -0800

    Exit on unrecoverable failure

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,9 @@
+yarn test || exit 1
 branchName=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 echo $branchName
-git stash push -- src
+git stash push -- src || exit 1
 git checkout master
-yarn run build
+yarn run build || exit 1
 touch build/.nojekyll
 git add docs
 git commit -m "Update docs for release $1"
