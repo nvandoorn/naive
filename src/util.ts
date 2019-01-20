638ff4686f557a2f9c31c521d849360fc906ca52
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-01-20 13:41:18 -0800

    Implement helper to generate random key

--- src/util.ts
+++ src/util.ts
@@ -2,3 +2,9 @@
  * Get the last item in t
  */
 export const last = <T>(t: T[]): T => t[t.length - 1];
+
+/**
+ * Generate a random key with a seed
+ */
+export const getKey = (seed: string) =>
+  seed + Date.now() + Math.floor(Math.random() * 1000);
