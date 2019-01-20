2e94d8dce0fb509afd8cdb795067d97ecc4c364f
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-01-20 15:21:34 -0800

    MONOREPO

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,4 @@
+sed -i.bak s/__REPLACE_VERSION__/$1/g package.json
 yarn test || exit 1
 branchName=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 echo $branchName
@@ -10,10 +11,10 @@ yarn run build || exit 1
 # to serve paths that start
 # with '_'
 touch docs/.nojekyll
-git add docs
+git add docs package.json
 git commit -m "Update docs for release $1"
 git tag $1
 git push --tags origin master
 git checkout $branchName
 git stash pop
-
+# sed -i.bak s/$1/__REPLACE_VERSION__/g package.json
