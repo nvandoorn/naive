034e9cf88bdcf8209342f04e459fab9efb75700f
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-01-19 21:32:24 -0800

    Comment on release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,14 @@
 yarn test || exit 1
 branchName=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 echo $branchName
+# TODO find a better way to exlcude
+# docs from the stash
 git stash push -- src || exit 1
 git checkout master
 yarn run build || exit 1
+# this is a hack required
+# to serve paths that start
+# with '_'
 touch docs/.nojekyll
 git add docs
 git commit -m "Update docs for release $1"
