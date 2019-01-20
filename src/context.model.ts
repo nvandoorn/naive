2115c6d5e62a4ccd89826e4781dd7e0ed98548d3
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-01-20 13:37:28 -0800

    Document context interface

--- src/context.model.ts
+++ src/context.model.ts
@@ -1,5 +1,18 @@
 export interface Context {
+  /**
+   * Method used to log.
+   *
+   * console.log is usually supplied
+   * but it may be handy to pipe it elsewhere
+   * later
+   */
   logger: (e: any) => void;
+  /**
+   * Location of the database cache file (JSON file)
+   */
   cachePath: string;
+  /**
+   * Maximum database size in megabytes
+   */
   maxDbSizeMB: number;
 }
