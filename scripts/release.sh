7bbacce30d6717228d39f787103f5b72f4eb7bfa
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-01-19 21:15:25 -0800

    Implement release script

---
+++ scripts/release.sh
@@ -0,0 +1,13 @@
+branchName=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+echo $branchName
+git stash push -- src
+git checkout master
+yarn run build
+touch build/.nojekyll
+git add docs
+git commit -m "Update docs for release $1"
+git tag $1
+git push origin master
+git checkout $branchName
+git stash pop
+
